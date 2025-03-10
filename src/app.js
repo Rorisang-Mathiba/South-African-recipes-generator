3fa8893cf1e69b652b8dece51b2fadb2901eacfe
Rorisang-Mathiba <rmathiba71@gmail.com>
2025-03-10 16:41:26 +0200

    Added the API Integration

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
+  console.log("recipe generated");
   new Typewriter("#recipe", {
-    strings: "The recipe will go here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "0f33dc651394et8090bcbf29o612a0f7";
+  let prompt = `User instructions: Generate one South African food recipes about ${instructionsInput.value}`;
+  let context =
+    " You are a South African food expect and love to write short and easy recipes. Your mission is to generate recipes in basic HTML. Make sure to follow the user instructions. Also just say the recipe. Don't add the html text";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
